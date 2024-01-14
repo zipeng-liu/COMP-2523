67f7afebe2ca9c81588830975b7e328cccc329ea
denis <denis961111@gmail.com>
2024-01-14 15:10:45 -0800

    Add file

---
+++ Class1/app.ts
@@ -0,0 +1,13 @@
+function square(a: number): number {
+  return a ** 2;
+}
+
+function squareRoot(b: number) {
+  return b ** 0.5;
+}
+
+function distance(x1: number, y1: number, x2: number, y2: number): number {
+	return squareRoot(square(x2 - x1) + square(y2 - y1))
+}
+
+console.log(distance(2, 3, 4, 6));
